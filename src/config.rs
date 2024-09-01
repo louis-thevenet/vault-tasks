3895b33815425bf61bc229b1c085a6693fd1a6eb
Louis Thevenet <louis.tvnt@gmail.com>
2024-09-01 22:57:26 +0200

    feat: add config option for american date format

--- src/config.rs
+++ src/config.rs
@@ -1,32 +1,32 @@
 use anyhow::{bail, Context, Result};
 
-use log::info;
+use log::{debug, info};
 use serde::Deserialize;
 use std::{fs::read_to_string, path::PathBuf};
 
 use crate::Args;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 pub struct Config {
     pub vault_path: PathBuf,
+    /// Default is true
+    pub use_american_format: Option<bool>,
 }
-
 impl Config {
     pub fn load_config(args: &Args) -> Result<Config> {
         let config_path = if let Some(path) = args.config_path.clone() {
             path
+        } else if let Some(user_dirs) = directories::UserDirs::new() {
+            user_dirs.home_dir().to_path_buf()
         } else {
-            if let Some(user_dirs) = directories::UserDirs::new() {
-                user_dirs.home_dir().to_path_buf()
-            } else {
-                bail!("Could not find config path");
-            }
+            bail!("Could not find config path");
         };
         let config_file = config_path.join(".vault-tasks.toml");
         info!("Loading config from {:?}", config_file);
 
         let content = read_to_string(&config_file).context("Failed to read config file")?;
         let config = toml::from_str::<Config>(&content).context("Failed to parse config file")?;
+        debug!("{:#?}", config);
 
         if !config.vault_path.exists() {
             bail!("Vault path {:?} not found.", config.vault_path);
