ca213da02e0eee3e31528d70939bc3548b8798bc
Louis Thevenet <louis.tvnt@gmail.com>
2024-09-04 20:49:10 +0200

    feat: improve FileEntry's to_string

--- src/core.rs
+++ src/core.rs
@@ -12,6 +12,24 @@ pub enum FileEntry {
 
 impl Display for FileEntry {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn write_indent(indent_length: usize, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            (1..=indent_length).try_for_each(|_| write!(f, "\t"))?;
+            Ok(())
+        }
+        fn write_underline_with_indent(
+            text: &str,
+            indent_length: usize,
+            f: &mut std::fmt::Formatter,
+        ) -> std::fmt::Result {
+            write_indent(indent_length, f)?;
+            writeln!(f, "{text}")?;
+            write_indent(indent_length, f)?;
+            for _i in 0..(text.len()) {
+                write!(f, "‾")?;
+            }
+            writeln!(f)?;
+            Ok(())
+        }
         fn fmt_aux(
             file_entry: &FileEntry,
             f: &mut std::fmt::Formatter,
@@ -19,21 +37,20 @@ impl Display for FileEntry {
         ) -> std::fmt::Result {
             match file_entry {
                 FileEntry::Header(header, entries) => {
-                    (0..depth).try_for_each(|_| write!(f, "\t"))?;
-                    writeln!(f, "{header}")?;
+                    write_underline_with_indent(&format!("{depth}. {header}"), depth, f)?;
                     for entry in entries {
                         fmt_aux(entry, f, depth + 1)?;
                     }
                 }
                 FileEntry::Task(task, subtasks) => {
                     for line in task.to_string().split('\n') {
-                        (0..=depth).try_for_each(|_| write!(f, "\t"))?;
+                        write_indent(depth, f)?;
                         writeln!(f, "{line}")?;
                     }
 
                     for subtask in subtasks {
                         for line in subtask.to_string().split('\n') {
-                            (0..=depth + 1).try_for_each(|_| write!(f, "\t"))?;
+                            write_indent(depth + 1, f)?;
                             writeln!(f, "{line}")?;
                         }
                     }
