3824cb0d90bb229b9cfcb0b2a0a74ced5b7995fd
Louis Thevenet <louis.tvnt@gmail.com>
2024-10-10 16:22:29 +0200

    tests(searchbar): add tests for search bar widget

--- src/widgets/search_bar.rs
+++ src/widgets/search_bar.rs
@@ -27,3 +27,59 @@ impl<'a> WidgetRef for SearchBar<'a> {
         .render(area, buf);
     }
 }
+#[cfg(test)]
+mod tests {
+    use insta::assert_snapshot;
+    use ratatui::{
+        backend::TestBackend,
+        layout::{Constraint, Layout},
+        widgets::Block,
+        Terminal,
+    };
+    use tui_input::Input;
+
+    use crate::widgets::search_bar::SearchBar;
+
+    #[test]
+    fn test_render_search_bar() {
+        let bar = SearchBar {
+            input: Input::new("input".to_owned()),
+            is_focused: true,
+            block: Some(Block::bordered().title_top("test")),
+        };
+        let mut terminal = Terminal::new(TestBackend::new(80, 20)).unwrap();
+        terminal
+            .draw(|frame| frame.render_widget(&bar, frame.area()))
+            .unwrap();
+        assert_snapshot!(terminal.backend());
+    }
+    #[test]
+    fn test_render_search_bar_line() {
+        let input = Input::new("initial".to_owned());
+        let bar = SearchBar {
+            input,
+            is_focused: true,
+            block: Some(Block::bordered().title_top("test")),
+        };
+        let mut terminal = Terminal::new(TestBackend::new(80, 20)).unwrap();
+        terminal
+            .draw(|frame| {
+                let [_, inner, _] = Layout::vertical([
+                    Constraint::Percentage(40),
+                    Constraint::Min(1),
+                    Constraint::Percentage(40),
+                ])
+                .areas(frame.area());
+                let [_, inner, _] = Layout::horizontal([
+                    Constraint::Percentage(20),
+                    Constraint::Min(10),
+                    Constraint::Percentage(20),
+                ])
+                .areas(inner);
+
+                frame.render_widget(&bar, inner);
+            })
+            .unwrap();
+        assert_snapshot!(terminal.backend());
+    }
+}
