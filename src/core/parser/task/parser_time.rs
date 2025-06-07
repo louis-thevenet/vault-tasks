4841be246cff467c8bfca857e1b2b143d31f944f
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-07 09:43:47 +0200

    fix(core): strengthen time parsing rules (don't accept h>24 etc)

--- src/core/parser/task/parser_time.rs
+++ src/core/parser/task/parser_time.rs
@@ -1,18 +1,22 @@
 use chrono::NaiveTime;
-use winnow::{combinator::separated, token::take_while, Parser, Result};
+use winnow::{combinator::separated, prelude::*, token::take_while, Result};
 
 use super::token::Token;
 
 /// Parses a `NaiveTime` from a `hh:mm:ss` or `hh:mm` string.
 pub fn parse_naive_time(input: &mut &str) -> Result<Token> {
-    let tokens: Vec<u32> =
-        separated(2..=3, take_while(1.., '0'..='9').parse_to::<u32>(), ':').parse_next(input)?;
-
-    let h = tokens[0];
-    let m = tokens[1];
-    let s = if tokens.len() == 3 { tokens[2] } else { 0 };
-
-    Ok(Token::DueTime(NaiveTime::from_hms_opt(h, m, s).unwrap()))
+    separated(2..=3, take_while(1.., '0'..='9').parse_to::<u32>(), ':')
+        .verify_map(|tokens: Vec<u32>| {
+            let h = tokens[0];
+            let m = tokens[1];
+            let s = if tokens.len() == 3 { tokens[2] } else { 0 };
+            if h < 24 && m < 60 && s < 60 {
+                Some(Token::DueTime(NaiveTime::from_hms_opt(h, m, s).unwrap()))
+            } else {
+                None
+            }
+        })
+        .parse_next(input)
 }
 
 #[cfg(test)]
@@ -21,6 +25,15 @@ mod tests {
 
     use crate::core::parser::task::{parser_time::parse_naive_time, token::Token};
 
+    #[test]
+    fn test_parse_wrong_naive_time() {
+        let input = "24:50".to_string();
+        assert!(parse_naive_time(&mut input.as_str()).is_err());
+        let input = "22:90".to_string();
+        assert!(parse_naive_time(&mut input.as_str()).is_err());
+        let input = "23:50:61".to_string();
+        assert!(parse_naive_time(&mut input.as_str()).is_err());
+    }
     #[test]
     fn test_parse_naive_time() {
         let now = chrono::Local::now().time();
