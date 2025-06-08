ba2c9707324541405781932c3e834d4222bd0aad
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-08 19:56:30 +0200

    feat: implement Display for Tracker

--- src/core/tracker.rs
+++ src/core/tracker.rs
@@ -1,16 +1,45 @@
+use std::fmt::Display;
+
+use frequency::Frequency;
+use tabled::{builder::Builder, settings::Style};
 use tracker_category::TrackerCategory;
 
 use super::date::Date;
+mod frequency;
 mod tracker_category;
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Tracker {
     /// Name of the tracker
     name: String,
     /// Date of the first occurrence
     start_date: Date,
-    /// Frequency in seconds, (`start_date` + `frequency` * `n` = n-th occurrence)
-    frequency: usize,
+    /// Total number of occurrences
+    length: usize,
+    /// Frequency (`start_date` + `frequency` * `n` = n-th occurrence)
+    frequency: Frequency,
     /// Categories of the tracker
     categories: Vec<TrackerCategory>,
+}
+
+impl Display for Tracker {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut b = Builder::new();
+        b.push_record(
+            vec![
+                self.categories.iter().map(|c| c.name.clone()).collect(),
+                vec!["Notes".to_string()],
+            ]
+            .concat(),
+        );
+
+        for n in 0..self.length {
+            b.push_record(
+                self.categories
+                    .iter()
+                    .map(|c| c.entries.get(n).unwrap().to_string()),
+            );
+        }
+
+        write!(f, "{}", b.build().with(Style::markdown()).to_string())
+    }
 }
--- src/core/tracker/tracker_category.rs
+++ src/core/tracker/tracker_category.rs
@@ -1,11 +1,24 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ScoreEntry {
     pub score: i32,
+}
+impl Display for ScoreEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.score)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BoolEntry {
     pub value: bool,
+}
+
+impl Display for BoolEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.value)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -20,6 +33,15 @@ pub enum EntryType {
     Bool(BoolEntry),
 }
 
+impl Display for EntryType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EntryType::Score(entry) => write!(f, "{}", entry),
+            EntryType::Bool(entry) => write!(f, "{}", entry),
+        }
+    }
+}
+
 impl EntryType {
     fn variant(&self) -> EntryTypeVariant {
         match self {
@@ -30,19 +52,11 @@ impl EntryType {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct TrackerEntry {
-    /// There is always a notes field in a tracker category
-    note: String,
-    /// Entry
-    entry: EntryType,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TrackerCategory {
     /// Name of the tracker category
-    name: String,
+    pub name: String,
     /// The type of entries this category accepts
     entry_type: EntryTypeVariant,
     /// Entries in this tracker category
-    entries: Vec<TrackerEntry>,
+    pub entries: Vec<EntryType>,
 }
