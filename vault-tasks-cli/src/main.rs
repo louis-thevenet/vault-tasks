62e825adf8f807b8c9818aba45574f7eade7de0e
Louis Thevenet <louis.tvnt@gmail.com>
2025-08-10 10:09:53 +0200

    feat: boilerplate code

--- vault-tasks-cli/src/main.rs
+++ vault-tasks-cli/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+mod errors;
+
+use color_eyre::Result;
+use vault_tasks_core::init_logging;
+
+fn main() -> Result<()> {
+    crate::errors::init()?;
+    init_logging()?;
+
     println!("Hello, world!");
+    Ok(())
 }
