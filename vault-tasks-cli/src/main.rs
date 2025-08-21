766da7de092eb62f4f709dd161d72d46c7a9c205
Louis Thevenet <louis.tvnt@gmail.com>
2025-08-21 10:36:57 +0200

    wip: waiting for add_task refactor

--- vault-tasks-cli/src/main.rs
+++ vault-tasks-cli/src/main.rs
@@ -4,17 +4,52 @@ mod errors;
 
 use clap::Parser;
 use cli::Cli;
-use color_eyre::Result;
+use color_eyre::{Result, eyre::bail};
 use config::Config;
-use vault_tasks_core::init_logging;
+use vault_tasks_core::{TaskManager, init_logging};
 
 fn main() -> Result<()> {
     crate::errors::init()?;
     init_logging()?;
 
-    let cli = Cli::parse();
-    println!("{cli:#?}");
+    let args = Cli::parse();
     let config = Config::new(&args)?;
-    println!("{:#?}", config);
+    println!("{config:#?}");
+    match args.command {
+        cli::Commands::List {
+            file_selector_args,
+            filter_args,
+        } => println!(
+            "Listing tasks with file selector args: {file_selector_args:?} and filter args: {filter_args:?}"
+        ),
+        cli::Commands::Add { task, args } => {
+            if args.path.len() > 1 {
+                bail!(
+                    "Can't decide where to add new task when multiple paths are provided: {:?}",
+                    args.path
+                );
+            }
+
+            
+            
+            let mut task_manager = TaskManager::load_from_config(&config.core)?;
+            task_manager.add_task(
+                &task,
+                args.path.first().and_then(|path| {
+                    // TODO: refactor add_task to take paths
+                    path.file_name()
+                        .map(|filename| filename.to_string_lossy().into_owned())
+                }),
+            );
+        }
+
+        cli::Commands::Mark {
+            new_state,
+            file_selector_args,
+            filter_args,
+        } => println!(
+            "Marking tasks with new state: {new_state:?}, file selector args: {file_selector_args:?}, and filter args: {filter_args:?}"
+        ),
+    }
     Ok(())
 }
