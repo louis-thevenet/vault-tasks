592753734e8080acfca2af94e03e244d0c0d7ef3
Louis Thevenet <louis.tvnt@gmail.com>
2025-08-21 10:23:45 +0200

    feat: add boilerplate config handling

--- vault-tasks-cli/src/main.rs
+++ vault-tasks-cli/src/main.rs
@@ -1,9 +1,11 @@
 mod cli;
+mod config;
 mod errors;
 
 use clap::Parser;
 use cli::Cli;
 use color_eyre::Result;
+use config::Config;
 use vault_tasks_core::init_logging;
 
 fn main() -> Result<()> {
@@ -12,5 +14,7 @@ fn main() -> Result<()> {
 
     let cli = Cli::parse();
     println!("{cli:#?}");
+    let config = Config::new(&args)?;
+    println!("{:#?}", config);
     Ok(())
 }
