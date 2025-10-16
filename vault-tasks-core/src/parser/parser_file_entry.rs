08b36e9eb41d90916a4de326cd9299486be13775
mstorick <96434946+mstorick@users.noreply.github.com>
2025-10-16 09:37:42 -0500

    fix: tui artifacts by removing carriage returns (#145)
    * Fix tui artifacts by removing carriage returns
    
    * Resolve Clippy
    
    Co-authored-by: Louis Thevenet <louis.thevenet@proton.me>
    
    ---------
    
    Co-authored-by: Louis Thevenet <louis.thevenet@proton.me>

--- vault-tasks-core/src/parser/parser_file_entry.rs
+++ vault-tasks-core/src/parser/parser_file_entry.rs
@@ -721,7 +721,8 @@ impl ParserFileEntry<'_> {
     }
 
     pub fn parse_file(&mut self, filename: &str, input: &&str) -> Option<VaultData> {
-        let lines = input.split('\n');
+        let replaced = input.replace('\r', "");
+        let lines = replaced.split('\n');
 
         let mut res = VaultData::Header(0, filename.to_owned(), vec![]);
         let mut file_tags = vec![];
